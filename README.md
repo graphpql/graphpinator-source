bf2032c7ef88016f11d5eae43ea093691d0640e7
Václav Pelíšek <peldax@gmail.com>
2021-06-01 20:05:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GraPHPinator Source [![PHP](https://github.com/infinityloop-dev/graphpinator-source/workflows/PHP/badge.svg?branch=master)](https://github.com/infinityloop-dev/graphpinator-source/actions?query=workflow%3APHP) [![codecov](https://codecov.io/gh/infinityloop-dev/graphpinator-source/branch/master/graph/badge.svg)](https://codecov.io/gh/infinityloop-dev/graphpinator-source)
+
+:zap::globe_with_meridians::zap: Source subproject for GraPHPinator: GraphQL server implementation for PHP.
---
+++ src/Exception/UnexpectedEnd.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Graphpinator\Source\Exception;
+
+final class UnexpectedEnd extends \Graphpinator\Exception\GraphpinatorBase
+{
+    public const MESSAGE = 'Unexpected end of input. Probably missing closing brace?';
+
+    public function __construct(\Graphpinator\Common\Location $location)
+    {
+        parent::__construct();
+
+        $this->location = $location;
+    }
+
+    public function isOutputable() : bool
+    {
+        return true;
+    }
+}
